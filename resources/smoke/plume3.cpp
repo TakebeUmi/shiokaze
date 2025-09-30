0a954093a13880c8dcd44249d3a1d2d72d39257f
takebe <take.drawal@gmail.com>
2025-09-30 23:24:15 +0900

    configureにとりあえず丸コピ

--- resources/smoke/plume3.cpp
+++ resources/smoke/plume3.cpp
@@ -23,11 +23,32 @@
 */
 //
 #include <shiokaze/math/vec.h>
+#include <shiokaze/core/configuration.h>
 #include <map>
 #include <string>
 #include <cmath>
 //
 SHKZ_USING_NAMESPACE
+static double g_container_thickness (0.03);
+static double g_container_radius (0.5);
+static double g_container_height (0.3);
+static vec3d g_center (0.5,0.37,0.5);
+static double g_radius (0.075);
+static double g_level (0.245);
+static bool g_container (false);
+
+extern "C" void configure( configuration &config ) {
+	configuration::auto_group group(config,"Waterdrop Scene 3D","Waterdrop");
+	config.get_bool("Container",g_container,"Whether to place g_container");
+	if( g_container ) {
+		config.get_double("ContainerRadius",g_container_radius,"Radius of the solid hemisphere container");
+		config.get_double("ContainerThickness",g_container_thickness,"Thickness of the solid hemisphere container");
+		config.get_double("ContainerHeight",g_container_height,"Height of the solid hemisphere container");
+	}
+	config.get_double("Radius",g_radius,"Radius of water");
+	config.get_vec3d("Center",g_center.v,"g_center of spherical water");
+	config.get_double("Level",g_level,"Level of static water pool");
+}
 //
 extern "C" std::map<std::string,std::string> get_default_parameters () {
 	std::map<std::string,std::string> dictionary;
